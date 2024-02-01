db670e000cb26f5a6e3d093a4b05a03c9e7790dd
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-01 21:29:10 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] sayilar = new int[3];
+sayilar[0]= 100;
+sayilar[1] = 50;
+sayilar[2] = 15;
+
+foreach (var sayi in sayilar)
+{
+    Console.WriteLine(sayi);
+}
+
+string[] aylar = { "Ocak", "Şubat", "Mart", "Nisan", "Mayıs" };
+
+foreach (var ay in aylar)
+{
+    Console.WriteLine(ay);
+}
+Console.Read();
